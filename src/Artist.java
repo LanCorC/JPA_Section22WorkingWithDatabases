b4b9ff99c56d98104c38ecbac8b26221011f56e1
LanCorC <lanc3.corcega@gmail.com>
2024-05-15 16:23:59 +1200

    Section 22.372(Working with Databases: Introduction to JPA and ORM): introduction to JPA, ORM concepts
    writing up demonstration of an Entity

--- src/Artist.java
+++ src/Artist.java
@@ -14,5 +14,31 @@ public class Artist {
     @Column
     private String artistName;
 
-    
+    public Artist() {
+    }
+
+    public Artist(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public Artist(int artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artistId=" + artistId +
+                ", artistName='" + artistName + '\'' +
+                '}';
+    }
 }
