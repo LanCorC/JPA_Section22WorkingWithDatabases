cf1eeb44ed08b278fc44f3f1e0f04edea7835ad1
LanCorC <lanc3.corcega@gmail.com>
2024-05-16 16:07:00 +1200

    Section 22.378(Working with Databases: JPA Challenge): Bonus challenge progress so far 2.
    not much changed; there are '18' songs matching for '%Soul%'. noticed that JPQL would output '18', when there are only 12 artists. It was a SELECT * FROM artists, so it would duplicate some artists.
    
    still cannot resolve CriteriaBuilder to select for only Artists of Songs where an Album had a Song matching '%Soul%'

--- src/SongQuery.java
+++ src/SongQuery.java
@@ -10,17 +10,19 @@ import music.Artist;
 import music.Song;
 
 import java.util.List;
+import java.util.TreeSet;
 import java.util.stream.Stream;
 
 public class SongQuery {
     public static void main(String[] args) {
-        List<Tuple> allCandidates = null;
+        List<Artist> allCandidates = null;
         try (var emf = Persistence.createEntityManagerFactory("dev.lpa.music");
              EntityManager em = emf.createEntityManager()
         ) {
             var transaction = em.getTransaction();
 //            allCandidates = getArtistsWithSongJPQLstreamlined(em, "%Soul%");
             allCandidates = getArtistsWithSongBuilder(em, "%Soul%");
+//            allCandidates = getArtistsWithSongJPQL(em, "%Soul%");
             allCandidates.forEach(System.out::println);
             System.out.println("Output size: " + allCandidates.size());
             transaction.commit();
@@ -31,7 +33,8 @@ public class SongQuery {
     }
 
     public static List<Artist> getArtistsWithSongJPQL(EntityManager em, String matchedValue) {
-        String jpql = "SELECT a FROM Artist a JOIN albums album JOIN songs song WHERE song.songTitle LIKE ?1";
+        String jpql = "SELECT DISTINCT a FROM Artist a JOIN albums album JOIN songs song WHERE song.songTitle LIKE ?1";
+//        String jpql = "SELECT a.artistName, album.albumName, song.songTitle FROM Artist a JOIN albums album JOIN songs song WHERE song.songTitle LIKE ?1";
         Query qr = em.createQuery(jpql, Artist.class);
         qr.setParameter(1, matchedValue);
         return qr.getResultList();
@@ -45,18 +48,17 @@ public class SongQuery {
     }
 
     //Bonus challenge:
-    public static List<Tuple> getArtistsWithSongBuilder(EntityManager em, String matchedValue) {
+    public static List<Artist> getArtistsWithSongBuilder(EntityManager em, String matchedValue) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Tuple> cq = cb.createQuery(Tuple.class);
+        CriteriaQuery<Artist> cq = cb.createQuery(Artist.class);
         Root<Artist> rootArt = cq.from(Artist.class);
         Root<Album> rootAlb = cq.from(Album.class);
         Root<Song> rootSon = cq.from(Song.class);
-        cq.multiselect(rootArt.get("artistName"),rootAlb.get("albumName"),rootSon.get("songTitle")).
-                where(cb.like(rootSon.get("songTitle"), matchedValue));
+//        cq.multiselect(rootArt.get("artistName"),rootAlb.get("albumName"),rootSon.get("songTitle")).
+//                where(cb.like(rootSon.get("songTitle"), matchedValue));
+        cq.select(rootArt)
+                .where(cb.like(rootSon.get("songTitle"), matchedValue));
         return em.createQuery(cq).getResultList();
     }
-
-
-
 
 }
